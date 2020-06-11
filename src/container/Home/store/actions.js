a68f90585a36ba4834c8f29467ad79c1cd4a947f
fanluping <fanluping@bytedance.com>
2020-06-11 17:34:02 +0800

    feat: pass axiosInstance to 获取后端数据的接口

--- src/container/Home/store/actions.js
+++ src/container/Home/store/actions.js
@@ -1,16 +1,13 @@
 import { CHANGE_LIST } from './constants'
-import { instance as clientAxios } from '../../../client/request'
-import { instance as serverAxios } from '../../../server/request'
 
 const changeList = list => ({
   type: CHANGE_LIST,
   list,
 })
 
-export const getHomeList = server => {
-  const request = server ? serverAxios : clientAxios
-  return dispatch => {
-    return request.get('/api/news.json?secret=PP87ANTIPIRATE').then(res => {
+export const getHomeList = () => {
+  return (dispatch, getState, axiosInstance) => {
+    return axiosInstance.get('/api/news.json?secret=PP87ANTIPIRATE').then(res => {
       const list = res.data.data
       dispatch(changeList(list))
     })
--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,26 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux'
 import thunk from 'redux-thunk'
 import { reducer as homeReducer } from '../container/Home/store'
+import { instance as clientAxios } from '../client/request'
+import { instance as serverAxios } from '../server/request'
 
 const reducer = combineReducers({
   home: homeReducer,
 })
 
 export const getStore = () => {
-  return createStore(reducer, applyMiddleware(thunk))
+  // 改变服务器端store的内容, 那么就一定要使用serverAxios
+  return createStore(
+    reducer,
+    applyMiddleware(thunk.withExtraArgument(serverAxios)),
+  )
 }
 export const getClientStore = () => {
+  // 改变服务器端store的内容, 那么就一定要使用clientAxios
   const defaultState = window.context.state
-  return createStore(reducer, defaultState, applyMiddleware(thunk))
+  return createStore(
+    reducer,
+    defaultState,
+    applyMiddleware(thunk.withExtraArgument(clientAxios)),
+  )
 }
-
-// export default getStore
